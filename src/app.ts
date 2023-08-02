ac9752ef3694a99103e07980668c18ef0921b407
KaminskySergey <kaminsky_s_s@ukr.net>
2023-08-02 23:45:40 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,6 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
 
 app.use('/api', routes);
 
