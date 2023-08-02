f9c2dba416bfab2074a2e14939237bd9f8a84b7b
KaminskySergey <kaminsky_s_s@ukr.net>
2023-08-02 23:31:54 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -12,9 +12,3 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
-
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
